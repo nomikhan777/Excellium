c28f915fc23945a7aaa6c75112c384ea3de915ae
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-03-05 15:25:18 +0500

    first commit

---
+++ assignment1/Q3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+    cout << "Enter two integers: ";
+    cin >> num1 >> num2;
+
+    int result = gcd(num1, num2);
+    cout << "The greatest common divisor of " << num1 << " and " << num2 << " is " << result << endl;
+
+    return 0;
+}
